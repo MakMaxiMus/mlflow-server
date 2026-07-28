fd394e7102914d0847caf66f3e4c35cf6d04bbba
MakMaxiMus <Guk380689871767@outlook.com>
2026-07-28 22:06:16 +0300

    Add MLflow server setup in app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+
+db_url = os.environ.get("DB_URL")
+if not db_url:
+    raise ValueError("DB_URL secret is not set!")
+
+port = os.environ.get("PORT", "10000")
+
+cmd = [
+    "mlflow", "server",
+    "--backend-store-uri", db_url,
+    "--default-artifact-root", "./mlartifacts",
+    "--host", "0.0.0.0",
+    "--port", port
+]
+
+subprocess.run(cmd)
